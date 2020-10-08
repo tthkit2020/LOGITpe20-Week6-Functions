0a84ef033d9ff34581c1c372a497bfa6d144e718
Julia Aasmaa <my_e_10@hotmail.com>
2020-10-08 14:24:56 +0300

    functions

---
+++ Params/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParamsSum(5, 5);
+            DisplayString("100");
+        }
+
+        //write a function that takes in two numbers and displays
+        //their sum as a result
+        public static void ParamsSum(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine(result);
+        }
+        //write a function that takes in a string as a parameter
+        //and displays it in the console
+        public static void DisplayString(string someString)
+        {
+            Console.WriteLine(someString);
+        }
+    }
+}
---
+++ Week6Functions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Week6Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //call a function
+            HelloUser();
+            Sum();
+        }
+
+        public static void HelloUser() //create a function
+        {
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Hello, {userName}");
+        }
+
+        public static void Sum()
+        {
+            Console.WriteLine("Enter the first number: ");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            int b = Int32.Parse(Console.ReadLine());
+
+            int result = a + b;
+            Console.WriteLine(result);
+        }
+
+    }
+}
